2597372e81a3dac2b1f277bd87e4ff21696338f4
terrencetec <terrencetec@gmail.com>
2020-09-20 21:54:23 +0800

    added documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,9 +51,10 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 
-import sphinx_pdj_theme
-html_theme = 'sphinx_pdj_theme'
-htm_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
+# import sphinx_pdj_theme
+# html_theme = 'sphinx_pdj_theme'
+# htm_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
+html_theme = 'classic'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
