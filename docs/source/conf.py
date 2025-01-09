7e763235b28321fcb92183109879db6732d0c949
terrencetec <terrencetec@gmail.com>
2025-01-09 14:36:22 -0600

    clean up and change to pyproject.toml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,8 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 
 project = 'mypythonlibrary'
-copyright = '2020, Tsang Terrence Tak Lun'
-author = 'Tsang Terrence Tak Lun'
+copyright = '2020-2025, Terrence Tsang'
+author = 'Terrence Tsang'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.0'
